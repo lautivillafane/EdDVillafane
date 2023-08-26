f3658c189f6f493aff5678285cbcb90fa30184b4
Lautaro Villafañe <lautaarovillafane@gmail.com>
2023-08-26 17:24:09 -0300

    Ej 2 Funciones

--- Ejercicios/01 22-08 funciones.py
+++ Ejercicios/01 22-08 funciones.py
@@ -10,5 +10,23 @@ def main():
     altura = int(input("Indique la altura del rectangulo: "))
     print(area_rectangulo(base,altura))
 
+# if __name__ == "__main__":
+#     main()
+
+"""Ejercicio 2.
+Realizar una función que se llame area_circulo() que devuelva el área del
+círculo a partir de su radio.
+Utilizar el valor 3.14159 como pi
+"""
+
+def area_circulo(radio):
+    pi = 3.14159
+    return pi*(radio**2)
+
+def main():
+    radio = int(input("Indique el radio del círculo: "))
+    area = area_circulo(radio)
+    print(f"El área del círculo es {area}")
+
 if __name__ == "__main__":
     main()
