d15db379290fc8a21cbe04827011e337349865dd
Lautaro Villafañe <lautaarovillafane@gmail.com>
2023-08-26 17:29:19 -0300

    Ej 3 funciones

--- Ejercicios/01 22-08 funciones.py
+++ Ejercicios/01 22-08 funciones.py
@@ -28,5 +28,28 @@ def main():
     area = area_circulo(radio)
     print(f"El área del círculo es {area}")
 
+# if __name__ == "__main__":
+#     main()
+
+"""Ejercicio 3.
+Realizar una función que se llame relacion() que a partir de dos nros realicce
+lo siguiente:
+Si el primer nro es mayor que el segundo, devuelva 1
+Si el primer nro es menor que el segundo, devuelva -1
+Si ambos nros son iguales, devuelva 0"""
+
+def relacion(num1, num2):
+    if num1 > num2:
+        return 1
+    elif num1 < num2:
+        return -1
+    else:
+        return 0
+
+def main():
+    num1 = int(input("Indique el primer número a comparar: "))
+    num2 = int(input("Indique el segundo número a comparar: "))
+    print(relacion(num1,num2))
+    
 if __name__ == "__main__":
     main()
